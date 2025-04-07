f01cde47949276a317db526e78198978a636fe36
lucabellantee <s1097955@studenti.univpm.it>
2025-04-08 00:05:57 +0200

    Create zephyr-env.cmd

---
+++ zephyr-env.cmd
@@ -0,0 +1,6 @@
+@echo off
+set ZEPHYR_BASE=C:\zephyrproject\zephyr
+set ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
+set GNUARMEMB_TOOLCHAIN_PATH=C:\Program Files (x86)\GNU Arm Embedded Toolchain\10 2021.10
+echo Ambiente Zephyr configurato!
+echo ZEPHYR_BASE = %ZEPHYR_BASE%
