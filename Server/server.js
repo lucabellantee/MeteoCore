c146f8c0d41c6987d105afd4b1b9539184e60c3c
lucabellantee <s1097955@studenti.univpm.it>
2025-04-13 19:08:55 +0200

    Aggiunto file .js per la creazione del server

---
+++ Server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/data', (req, res) => {
+  console.log('Dati ricevuti:', req.body);
+  res.status(200).send({ message: 'Dati ricevuti correttamente' });
+});
+
+app.listen(PORT, '192.168.83.5', () => {
+  console.log(`Server attivo su http://192.168.83.5:${PORT}`);
+});
+
